43312a5d91009714d9d0490bf948a9cf1f3f5806
highboi <highboi@protonmail.com>
2023-01-16 22:56:44 -0600

    Added code to procedurally generate grid layers to represent space-time in 3d. Need to figure out the math for 'attracting' the points to anywhere in the 3d volume. The distance would have to change and therefore some pythagorean theorem might be in order.

--- gwave.py
+++ gwave.py
@@ -5,14 +5,26 @@ import numpy as np
 #make a 3d graph to plot data
 spacetime = plt.figure().add_subplot(projection="3d")
 
+#a variable for the volume of the spacetime being simulated
+size = 100
+
 #generate the 2d mesh for graphing repeatedly into layers
-x, y = np.meshgrid(np.linspace(0, 100), np.linspace(0, 100))
+x, y = np.meshgrid(np.linspace(0, size), np.linspace(0, size))
 
-#calculate the z-coordinates based on the x and y coordinates
-z = x*y
 
-#plot the 3d data
-spacetime.plot_wireframe(x, y, z, rstride=20, cstride=20)
+#loop in order to graph multiple 2d layers into a 3d representation of space-time
+for elev in range(0, size, 5):
+	print(elev)
+
+	#make an empty array with the same shape as the x or y coordinates
+	z = np.empty(x.shape)
+	z.fill(0)
+
+	#add the elevation to the z coordinates
+	z += elev
+
+	#plot the 3d data
+	spacetime.plot_wireframe(x, y, z, rstride=5, cstride=5)
 
 #show the data on the graph
 plt.show()
